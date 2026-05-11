d5ad8fb3aede4481cae977b237c4af65d862efc5
cheesy <92267915+cheesyXD@users.noreply.github.com>
2026-05-11 20:22:45 +0100

    log in not working lol

---
+++ LabFusion/src/Network/Layers/LiteNetLib/LiteNetLibLayer.cs
@@ -0,0 +1,128 @@
+﻿using Il2CppSLZ.Bonelab;
+using LabFusion.Player;
+using LabFusion.Utilities;
+using LabFusion.Voice;
+using LabFusion.Voice.Unity;
+using LiteNetLib;
+using LiteNetLib.Utils;
+using System.Net;
+using System.Net.Sockets;
+using static Il2Cpp.Interop;
+
+namespace LabFusion.Network;
+
+public class LiteNetLibLayer : NetworkLayer
+{
+    public const int port = 7778;
+    public const string localAddress = "127.0.0.1";
+    private VoiceManager voiceManager;
+    public override IVoiceManager VoiceManager => voiceManager;
+    public LiteNetLibServer server { get; private set; }
+    public LiteNetLibClient client { get; private set; }
+    public override string Title => "LiteNetLib";
+    public override string Platform => "Empty";
+    public override bool CheckSupported()
+    {
+        return true;
+    }
+
+    public override bool CheckValidation()
+    {
+        return true;
+    }
+
+    public override void Disconnect(string reason = "")
+    {
+        Shutdown();
+    }
+    private void Shutdown()
+    {
+        client?.Shutdown();
+        server?.Shutdown();
+    }
+
+
+    public override void DisconnectUser(ulong platformID)
+    {
+        server.netManager.DisconnectPeer(server.netManager.GetPeerById((int)platformID));
+    }
+
+    public override void LogIn()
+    {
+    }
+
+    public override void LogOut()
+    {
+    }
+
+    public override void OnDeinitializeLayer()
+    {
+        voiceManager.Disable();
+        voiceManager = null;
+        Shutdown();
+        InvokeLoggedOutEvent();
+    }
+
+    public override void OnInitializeLayer()
+    {
+        server ??= new();
+        server.Initialize();
+        client ??= new();
+        client.Initialize();
+
+        voiceManager = new UnityVoiceManager();
+        voiceManager.Enable();
+        InvokeLoggedInEvent();
+    }
+    public override void OnUpdateLayer()
+    {
+        server.netManager.PollEvents();
+        client.netManager.PollEvents();
+    }
+    public override void StartServer()
+    {
+        server.Start(port);
+        client.Connect(localAddress, port);
+    }
+
+    public override void SendToServer(NetworkChannel channel, NetMessage message)
+    {
+        byte[] data = message.ToByteArray();
+        DeliveryMethod deliveryMethod = GetDeliveryMethod(channel);
+        client.peer.Send(data, deliveryMethod);
+    }
+
+    public override void SendFromServer(byte userId, NetworkChannel channel, NetMessage message)
+    {
+        var id = PlayerIDManager.GetPlayerID(userId);
+
+        if (id != null)
+        {
+            SendFromServer(id.PlatformID, channel, message);
+        }
+    }
+
+    public override void BroadcastMessage(NetworkChannel channel, NetMessage message)
+    {
+        byte[] data = message.ToByteArray();
+        DeliveryMethod deliveryMethod = GetDeliveryMethod(channel);
+
+        server.netManager.SendToAll(data, deliveryMethod);
+    }
+
+    public override void SendFromServer(ulong userId, NetworkChannel channel, NetMessage message)
+    {
+        byte[] data = message.ToByteArray();
+        DeliveryMethod deliveryMethod = GetDeliveryMethod(channel);
+
+        var peer = server.netManager.GetPeerById((int)userId);
+        peer.Send(data, deliveryMethod);
+    }
+
+    public override void JoinServerByCode(string address)
+    {
+        client.Connect(address, port);
+    }
+
+    public static DeliveryMethod GetDeliveryMethod(NetworkChannel channel) => channel == NetworkChannel.Reliable ? DeliveryMethod.ReliableOrdered : DeliveryMethod.Unreliable;
+}
---
+++ LabFusion/src/Network/Layers/LiteNetLib/LiteNetLibServer.cs
@@ -0,0 +1,59 @@
+﻿using LabFusion.Utilities;
+using LiteNetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFusion.Network;
+
+public class LiteNetLibServer : LiteNetLibCommon
+{
+    public override void OnConnectionRequest(ConnectionRequest request)
+    {
+        request.Accept();
+    }
+
+    public override void OnNetworkError(IPEndPoint endPoint, SocketError socketError)
+    {
+    }
+
+    public override void OnNetworkLatencyUpdate(NetPeer peer, int latency)
+    {
+        FusionLogger.Log($"Network Latency! Peer: {peer.Id} Latency: {latency}");
+    }
+
+    public override void OnNetworkReceive(NetPeer peer, NetPacketReader reader, byte channelNumber, DeliveryMethod deliveryMethod)
+    {
+        ReadableMessage message = new()
+        {
+            Buffer = reader.GetRemainingBytes(),
+            IsServerHandled = true,
+            PlatformID = (ulong?)peer.Id,
+        };
+        NativeMessageHandler.ReadMessage(message);
+        reader.Recycle();
+    }
+
+    public override void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+    {
+    }
+
+    public override void OnPeerConnected(NetPeer peer)
+    {
+        FusionLogger.Log($"Peer Connected: {peer.Id}");
+    }
+
+    public override void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+    {
+        FusionLogger.Log($"Peer Disconnected: {peer.Id}");
+    }
+
+    public void Start(int port)
+    {
+        netManager.Start(port);
+    }
+}
