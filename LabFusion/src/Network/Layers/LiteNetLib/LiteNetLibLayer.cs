b385d5fad75c14d08bd6b8bfaafbc35a951361df
cheesy <92267915+cheesyXD@users.noreply.github.com>
2026-05-12 19:17:02 +0100

    connection issues

--- LabFusion/src/Network/Layers/LiteNetLib/LiteNetLibLayer.cs
+++ LabFusion/src/Network/Layers/LiteNetLib/LiteNetLibLayer.cs
@@ -1,13 +1,14 @@
-﻿using Il2CppSLZ.Bonelab;
+﻿using Il2CppTMPro;
+using LabFusion.Menu;
 using LabFusion.Player;
+using LabFusion.Senders;
+using LabFusion.UI.Popups;
 using LabFusion.Utilities;
 using LabFusion.Voice;
 using LabFusion.Voice.Unity;
 using LiteNetLib;
-using LiteNetLib.Utils;
-using System.Net;
-using System.Net.Sockets;
-using static Il2Cpp.Interop;
+using System.Collections.Concurrent;
+using UnityEngine;
 
 namespace LabFusion.Network;
 
@@ -15,81 +16,171 @@ public class LiteNetLibLayer : NetworkLayer
 {
     public const int port = 7778;
     public const string localAddress = "127.0.0.1";
-    private VoiceManager voiceManager;
+    private IVoiceManager voiceManager = null;
     public override IVoiceManager VoiceManager => voiceManager;
-    public LiteNetLibServer server { get; private set; }
-    public LiteNetLibClient client { get; private set; }
+
+
+    internal static readonly ConcurrentQueue<Tuple<byte[], bool>> MessageQueue = new ConcurrentQueue<Tuple<byte[], bool>>();
+
+    internal static readonly ConcurrentQueue<Action> ActionQueue = new ConcurrentQueue<Action>();
+
     public override string Title => "LiteNetLib";
-    public override string Platform => "Empty";
-    public override bool CheckSupported()
-    {
-        return true;
-    }
 
-    public override bool CheckValidation()
-    {
-        return true;
-    }
+    public override string Platform => "P2P";
 
-    public override void Disconnect(string reason = "")
+    public override bool IsHost => LiteNetLibThreader.IsServerRunning;
+
+    public override bool IsClient => LiteNetLibThreader.IsClientConnected;
+
+    private string ServerCode
     {
-        Shutdown();
+        get; set;
     }
-    private void Shutdown()
+    public override bool CheckSupported() => true;
+    public override bool CheckValidation() => true;
+
+    // Riptide doesn't really have a way to add these features out of the box...
+    public override string GetUsername(ulong userId) => userId.ToString();
+    public override bool IsFriend(ulong userId) => false;
+
+    public override void LogIn() => InvokeLoggedInEvent();
+    public override void LogOut() => InvokeLoggedOutEvent();
+
+    public override void OnInitializeLayer()
     {
-        client?.Shutdown();
-        server?.Shutdown();
-    }
+        LiteNetLibThreader.StartThread();
+        HookRiptideEvents();
 
 
-    public override void DisconnectUser(ulong platformID)
-    {
-        server.netManager.DisconnectPeer(server.netManager.GetPeerById((int)platformID));
-    }
-
-    public override void LogIn()
-    {
-    }
-
-    public override void LogOut()
-    {
+        voiceManager = new UnityVoiceManager();
+        voiceManager.Enable();
     }
 
     public override void OnDeinitializeLayer()
     {
         voiceManager.Disable();
         voiceManager = null;
-        Shutdown();
-        InvokeLoggedOutEvent();
+
+        Disconnect();
+
+        LiteNetLibThreader.KillThread();
+
+        UnhookRiptideEvents();
     }
 
-    public override void OnInitializeLayer()
+    private void HookRiptideEvents()
     {
-        server ??= new();
-        server.Initialize();
-        client ??= new();
-        client.Initialize();
+        // Add server hooks
+        MultiplayerHooking.OnPlayerJoined += OnPlayerJoin;
+        MultiplayerHooking.OnPlayerLeft += OnPlayerLeave;
+        MultiplayerHooking.OnDisconnected += OnDisconnect;
+    }
 
-        voiceManager = new UnityVoiceManager();
-        voiceManager.Enable();
-        InvokeLoggedInEvent();
+    private void UnhookRiptideEvents()
+    {
+        // Remove server hooks
+        MultiplayerHooking.OnPlayerJoined -= OnPlayerJoin;
+        MultiplayerHooking.OnPlayerLeft -= OnPlayerLeave;
+        MultiplayerHooking.OnDisconnected -= OnDisconnect;
+    }
+
+    private void OnPlayerJoin(PlayerID id)
+    {
+        if (VoiceManager == null)
+        {
+            return;
+        }
+
+        if (!id.IsMe)
+        {
+            VoiceManager.GetSpeaker(id);
+        }
+    }
+
+    private void OnPlayerLeave(PlayerID id)
+    {
+        if (VoiceManager == null)
+        {
+            return;
+        }
+
+        VoiceManager.RemoveSpeaker(id);
+    }
+
+    private void OnDisconnect()
+    {
+        VoiceManager.ClearManager();
     }
     public override void OnUpdateLayer()
     {
-        server.netManager.PollEvents();
-        client.netManager.PollEvents();
-    }
-    public override void StartServer()
-    {
-        server.Start(port);
-        client.Connect(localAddress, port);
+        for (int i = 0; i < MessageQueue.Count; i++)
+        {
+            if (MessageQueue.TryDequeue(out Tuple<byte[], bool> messageTuple))
+            {
+                byte[] bytes = messageTuple.Item1;
+                bool isServerHandled = messageTuple.Item2;
+                ReadableMessage message = new();
+                message.Buffer = bytes;
+                message.IsServerHandled = isServerHandled;
+                NativeMessageHandler.ReadMessage(message);
+            }
+        }
+
+        for (int i = 0; i < ActionQueue.Count; i++)
+            if (ActionQueue.TryDequeue(out Action action))
+                action();
     }
 
-    public override void SendToServer(NetworkChannel channel, NetMessage message)
+    public override void StartServer()
+    {
+        LiteNetLibThreader.StartServer();
+    }
+
+    public override void Disconnect(string reason = "")
+    {
+        LiteNetLibThreader.Disconnect();
+    }
+
+    public override string GetServerCode() => ServerCode = localAddress;
+
+    public override void RefreshServerCode()
+    {
+        ServerCode = localAddress;
+        GUIUtility.systemCopyBuffer = ServerCode;
+
+        Notifier.Send(new Notification()
+        {
+            SaveToMenu = false,
+            Message = $"For security purposes, Look up your IP Address and Open a port on {port}!",
+            Type = NotificationType.INFORMATION,
+            PopupLength = 3,
+        });
+    }
+
+    public override void JoinServerByCode(string code)
+    {
+        if (code == "1") code = localAddress;
+        LiteNetLibThreader.ConnectToServer(code);
+    }
+
+    private static DeliveryMethod GetDeliveryMethod(NetworkChannel channel)
+    {
+        return channel switch
+        {
+            NetworkChannel.Reliable => DeliveryMethod.ReliableOrdered,
+            NetworkChannel.Unreliable => DeliveryMethod.Unreliable,
+            _ => throw new ArgumentOutOfRangeException(nameof(channel), channel, null),
+        };
+    }
+
+    public override void BroadcastMessage(NetworkChannel channel, NetMessage message)
     {
         byte[] data = message.ToByteArray();
         DeliveryMethod deliveryMethod = GetDeliveryMethod(channel);
-        client.peer.Send(data, deliveryMethod);
+
+        var messageTuple = new Tuple<byte[], DeliveryMethod, int, bool>(data, deliveryMethod, 0, true);
+
+        LiteNetLibThreader.ServerSendQueue.Enqueue(messageTuple);
     }
 
     public override void SendFromServer(byte userId, NetworkChannel channel, NetMessage message)
@@ -102,27 +193,34 @@ public class LiteNetLibLayer : NetworkLayer
         }
     }
 
-    public override void BroadcastMessage(NetworkChannel channel, NetMessage message)
-    {
-        byte[] data = message.ToByteArray();
-        DeliveryMethod deliveryMethod = GetDeliveryMethod(channel);
-
-        server.netManager.SendToAll(data, deliveryMethod);
-    }
-
     public override void SendFromServer(ulong userId, NetworkChannel channel, NetMessage message)
     {
         byte[] data = message.ToByteArray();
         DeliveryMethod deliveryMethod = GetDeliveryMethod(channel);
 
-        var peer = server.netManager.GetPeerById((int)userId);
-        peer.Send(data, deliveryMethod);
+        var messageTuple = new Tuple<byte[], DeliveryMethod, int, bool>(data, deliveryMethod, (int)userId, false);
+
+        LiteNetLibThreader.ServerSendQueue.Enqueue(messageTuple);
     }
 
-    public override void JoinServerByCode(string address)
+    public override void SendToServer(NetworkChannel channel, NetMessage message)
     {
-        client.Connect(address, port);
+        byte[] data = message.ToByteArray();
+        DeliveryMethod deliveryMethod = GetDeliveryMethod(channel);
+
+        var messageTuple = new Tuple<byte[], DeliveryMethod>(data, deliveryMethod);
+        if (IsHost)
+            NativeMessageHandler.ReadMessage(new()
+            {
+                Buffer = data,
+                IsServerHandled = true,
+            });
+        else
+            LiteNetLibThreader.ClientSendQueue.Enqueue(messageTuple);
     }
 
-    public static DeliveryMethod GetDeliveryMethod(NetworkChannel channel) => channel == NetworkChannel.Reliable ? DeliveryMethod.ReliableOrdered : DeliveryMethod.Unreliable;
+    public override void DisconnectUser(ulong platformID)
+    {
+        LiteNetLibThreader.KickPlayer(platformID);
+    }
 }
